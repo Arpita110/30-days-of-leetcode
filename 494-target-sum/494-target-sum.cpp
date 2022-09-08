6474bcf56415e3ed26a4a8194aa0607f6f29f1e7
Arpita kasaudhan <88442951+Arpita110@users.noreply.github.com>
2022-09-08 19:14:28 +0530

    Time: 17 ms (75.09%), Space: 9.1 MB (62.37%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -9,7 +9,8 @@ public:
             }
         }
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum;j++){ //j=0 because we have to consider element=0
+            for(int j=0;j<=sum;j++)
+            { 
                 if(arr[i-1]<=j){
                     dp[i][j]=(dp[i-1][j] + dp[i-1][j-arr[i-1]]);
                 }
@@ -21,12 +22,13 @@ public:
         return dp[n][sum];
     }
     int findTargetSumWays(vector<int>& nums, int diff) {
-        //similar as subset sum with given diff // diff is target here
-        //all +ve in s1 and -ve in s2
         int sum=0,n=nums.size();
-        sum=accumulate(nums.begin(),nums.end(),sum);
-        if(abs(diff)>sum || (sum+diff)%2!=0)return 0;//sum+diff has to be even then only s1 will be found 
-        int s=(sum+diff)/2;//(sum-diff)/2 also valid
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+        }
+        if(abs(diff)>sum || (sum+diff)%2!=0)return 0;
+        int s=(sum+diff)/2;
         return countSubsetSum(n,nums,s);
     }
 };
