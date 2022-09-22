a82ec78153693e6762eb56c1cb5bd34258323a03
Arpita kasaudhan <88442951+Arpita110@users.noreply.github.com>
2022-09-22 17:55:26 +0530

    Time: 386 ms (6.82%), Space: 45 MB (31.68%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-     vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1));
-      for(int i=0;i<=coins.size();i++)
-          dp[i][0] = 0;
-      for(int j=0;j<=amount;j++)
-          dp[0][j] = INT_MAX-1;
-      for(int j=1;j<=amount;j++)
-         if(j % coins[0] == 0)
-          dp[1][j] = j/coins[0];
-        else
-            dp[1][j] = INT_MAX-1;
-    for(int i=2;i<=coins.size();i++)
-    {
-        for(int j=1;j<=amount;j++)
-        {
-            if(coins[i-1] <= j)
-                dp[i][j] = min( 1+dp[i][j-coins[i-1]] , dp[i-1][j]);
-            else
-                dp[i][j] = dp[i-1][j];
+    vector<vector<int>> dp(coins.size(), vector<int>(amount+1));
+        for(int i=0;i<coins.size();i++){
+            dp[i][0] = 0;
         }
-    } 
-     if(dp[coins.size()][amount] == INT_MAX-1)   
-         return -1;
-     return dp[coins.size()][amount];   
+        for(int j=1;j<=amount;j++){
+            if(j%coins[0] == 0){
+                dp[0][j] = j/coins[0];
+            }
+            else dp[0][j] = INT_MAX;
+        }
+        for(int i=1;i<coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                int include = INT_MAX; 
+                if(j >= coins[i] && dp[i][j-coins[i]]!=INT_MAX){
+                    include = 1+dp[i][j-coins[i]];
+                }
+                //exclude
+                int exclude = dp[i-1][j];
+                dp[i][j]= min(include,exclude);
+            }
+        }
+        int result = dp[coins.size()-1][amount];
+        return result>=INT_MAX-1  ? -1 : result;  
 }
 };
