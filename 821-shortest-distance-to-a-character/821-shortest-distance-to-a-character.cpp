5cf614d0d1428a760425f50694b7b69ac121db6a
Arpita kasaudhan <88442951+Arpita110@users.noreply.github.com>
2022-09-25 23:43:26 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (60.33%) - LeetHub

---
+++ 821-shortest-distance-to-a-character/821-shortest-distance-to-a-character.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) 
+    {
+         vector<int> v , ans;
+        for(int i = 0 ; i < s.size() ;i++)
+        {
+            if(s[i] == c)
+                v.push_back(i);
+        }
+        int j = 0;
+        for(int i = 0 ; i < s.size() ;i++)
+        {
+            
+            if(j == 0)
+                ans.push_back(v[j] - i); 
+            else if(j >= v.size())
+                ans.push_back(i - v[j - 1]);
+            else
+                ans.push_back(min(v[j] - i , i - v[j - 1]));
+            if(s[i] == c)
+                j++;
+        }
+        return ans;
+    }
+};
